560b0e9165d3bd6cea44fd49687012a050838ba1
YuriyDrankin2021 <angel-of-death66@yandex.ru>
2025-03-10 13:46:00 +0300

    add mockito to project add sample API/DB tests with mockito

---
+++ src/test/java/db/HibernateMockUserTest.java
@@ -0,0 +1,38 @@
+package db;
+
+import com.github.javafaker.Faker;
+import db.models.User;
+import db.services.UserService;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+public class HibernateMockUserTest {
+    private static final Logger logger = LoggerFactory.getLogger(HibernateMockUserTest.class);
+    Faker faker = new Faker();
+    SoftAssertions softAssertions = new SoftAssertions();
+
+    @Spy
+    UserService userService;
+
+    @Test
+    @DisplayName("Create new user in db")
+    public void createMockUserInDbTest() {
+        logger.debug("Start test 'Create new user in db mock'");
+        User mockUser = new User(faker.name().firstName(), faker.number().numberBetween(1, 100));
+        Mockito.doReturn(mockUser).when(userService).saveUser(any(User.class));
+
+        User resultUser = userService.saveUser( new User(faker.name().firstName(), faker.number().numberBetween(1, 100)));
+        softAssertions.assertThat(resultUser).as("Check mock created user").isEqualTo(mockUser);
+        softAssertions.assertAll();
+    }
+}
