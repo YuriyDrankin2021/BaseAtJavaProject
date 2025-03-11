6e6cba80abdcf7c9aec817a2be5c90e6333a8bc2
YuriyDrankin2021 <angel-of-death66@yandex.ru>
2025-03-11 10:44:20 +0300

    add aspect v1

--- src/main/java/db/services/UserService.java
+++ src/main/java/db/services/UserService.java
@@ -3,6 +3,7 @@ package db.services;
 import db.dao.UserDao;
 import db.models.Auto;
 import db.models.User;
+import io.qameta.allure.Step;
 
 import java.util.List;
 
@@ -12,18 +13,22 @@ public class UserService {
     public UserService() {
     }
 
+    @Step("Get from db data of user with id = {id}")
     public User findUserById(int id) {
         return usersDao.getById(id);
     }
 
+    @Step("Create in db new User with data: {user}")
     public User saveUser(User user) {
         return usersDao.create(user);
     }
 
+    @Step("Delete in db new User with data: {user}")
     public void deleteUser(User user) {
         usersDao.delete(user);
     }
 
+    @Step("Update in db new User with data: {user}")
     public User updateUser(User user) {
         return usersDao.update(user);
     }
