63e8509060fd04085c121fd136e6f74bd589998e
YuriyDrankin2021 <angel-of-death66@yandex.ru>
2025-03-26 12:11:17 +0300

    allure works! with local result directory

--- src/main/java/helpers/Browser.java
+++ src/main/java/helpers/Browser.java
@@ -44,9 +44,9 @@ public class Browser {
             }});
             capabilities.setCapability("selenoid:options", selenoidOptions);
             //if run local
-//            Configuration.remote = "http://localhost:4444/wd/hub";
+            Configuration.remote = "http://localhost:4444/wd/hub";
             //if run in docker
-            Configuration.remote = "http://172.17.0.2:4444/wd/hub";
+//            Configuration.remote = "http://172.17.0.2:4444/wd/hub";
         }
         Configuration.headless = Boolean.parseBoolean(getProperty("headless"));
     }
